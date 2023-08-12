7c77f20f314b2cce2a0c0c87631ae7df83ec509b
Fadlyyyy <fadlyynasah86@f=gmail.com>
2023-08-12 21:03:20 +0700

    dom selection

--- dom selection/script.js
+++ dom selection/script.js
@@ -1,31 +1,31 @@
 // DOM selection
 // document.getElementById() -> element
 
-// const judul = document.getElementById("judul");
+const judul = document.getElementById("judul");
 
-// judul.style.color = "red";
-// judul.style.backgroundColor = "blue";
-// judul.innerHTML = "Fardli";
+judul.style.color = "red";
+judul.style.backgroundColor = "blue";
+judul.innerHTML = "Fardli";
 
 // document.getElementsByTagName() -> HTMLCollections
 
-// const p = document.getElementsByTagName("p");
+const p = document.getElementsByTagName("p");
 // p[0].style.backgroundColor = "lightblue";
 // p[1].style.backgroundColor = "lightblue";
 // p[2].style.backgroundColor = "lightblue";
 // p[3].style.backgroundColor = "lightblue";
 
-// for (let i = 0; i < p.length; i++) {
-//   p[i].style.backgroundColor = "lightblue";
-// }
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "lightblue";
+}
 
-// const h1 = ducoment.getElementsByTagName("h1")[0];
-// h1.style.fontSize = "50px";
+const h1 = document.getElementsByTagName("h1")[0];
+h1.style.fontSize = "50px";
 
-// document.getElementsByClassName() -> HTMLCollection
+// document.getElementsByClassName(); //-> HTMLCollection
 
-// const p1 = document.getElementsByClassName('p1')[0];
-// p1.innerHTML = 'ubah dari javascript';
+const p1 = document.getElementsByClassName("p1")[0];
+p1.innerHTML = "ubah dari javascript";
 
 // document.querySelector() -> element
 
@@ -57,6 +57,6 @@
 // const p4 = document.getElementByTagName ('p');
 // p4[3].style.backgroundColor = 'lightblue';
 
-const sectionB = document.querySelector("section#b");
-const p4 = sectionB.getElementsByTagName("p")[0];
-p4.style.backgroundColor = "orange";
+// const sectionB = document.querySelector("section#b");
+// const p4 = sectionB.getElementsByTagName("p")[0];
+// p4.style.backgroundColor = "orange";
